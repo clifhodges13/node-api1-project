e5714175b609400d940ea943df2ade3f197376fa
Clif <clifhodges13@gmail.com>
2019-12-04 20:40:32 -0600

    got POST request working :)

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const db = require("./data/db");
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the api')
   console.log({ "ip": req.ip})
 })
 
 // POST A USER
-// app.post('/api/users', (req, res) => {
-//   if (!req.body.name || !req.body.bio) {
-//     res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
-//   } else {
-//     db.insert(req.body)
-//       .then(newUser => res.json(newUser))
-//       .catch(err => res.status(500).json({errorMessage: "There was an error while saving the user to the database."}))
-//   }
-// })
+app.post('/api/users', (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+  } else {
+    db.insert(req.body)
+      .then(newUser => res.json(newUser))
+      .catch(err => res.status(500).json({errorMessage: "There was an error while saving the user to the database."}))
+  }
+})
 
 // GET ALL USERS
 app.get('/api/users', (req, res) => {
